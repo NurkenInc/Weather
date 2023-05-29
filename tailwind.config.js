fafe2653590b3ce3bdd2e0f3dc47803f34324800
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-05-29 23:29:20 +0600

    added tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
