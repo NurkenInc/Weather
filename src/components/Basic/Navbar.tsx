e1f4c014d832c2839377771ffb23f91a0e370084
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-05-29 23:33:12 +0600

    added navbar component

---
+++ src/components/Basic/Navbar.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import logo from '@/public/logo.svg';
+
+export default function Navbar() {
+  const isSignedIn = false;
+  
+  return (
+    <nav className='navbar'>
+      <div className='flex-center gap-2'>
+        <Image
+          src={logo}
+          alt='Website logotype'
+          width={40}
+          height={40}
+        />
+        <h3 className='font-bold'>Weather</h3>
+      </div>
+      <div className='flex-center'>
+        {isSignedIn ? (
+          <div className='flex-center gap-3'>
+            <button className='btn'>
+              Profile
+            </button>
+            <Image
+              src={logo}
+              alt='Website logotype'
+              width={40} 
+              height={40} 
+            />
+          </div>
+        ) : (
+          <div className='flex-center'>
+            <button className='btn'>
+              Sign in
+            </button>
+          </div>
+        )}
+      </div>
+    </nav>
+  )
+}
