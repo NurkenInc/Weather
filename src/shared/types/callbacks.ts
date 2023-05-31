7e24739eb3c22f6664bfaa36132858877acb7fd7
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-06-01 00:15:49 +0600

    declared interfaces for next-auth callback functions

--- src/shared/types/callbacks.ts
+++ src/shared/types/callbacks.ts
@@ -1,9 +1,12 @@
-import { Session } from 'next-auth';
+import { Session, Account, User } from 'next-auth';
+import { Profile } from '@shared/types/auth';
 
 export interface SessionProps {
   session: Session,
 }
 
 export interface SignInProps {
-
+  account: Account,
+  profile: Profile,
+  user: User,
 }
