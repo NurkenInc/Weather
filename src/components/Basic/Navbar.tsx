8cfeb00cd8b542e6e0c095582f670209faf08f38
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-06-04 23:32:33 +0600

    Added providers for signing in

--- src/components/Basic/Navbar.tsx
+++ src/components/Basic/Navbar.tsx
@@ -1,9 +1,23 @@
+'use client'
+
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
+
 import logo from '@/public/logo.svg';
+import { Provider } from '@shared/types/auth';
+import { signIn, signOut, useSession, getProviders, ClientSafeProvider } from 'next-auth/react';
 
 export default function Navbar() {
-  const isSignedIn = false;
+  const { data: session } = useSession();
+  const [providers, setProviders] = useState<any>(null);
   
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })()
+  }, [])
+
   return (
     <nav className='navbar'>
       <div className='flex-center gap-2'>
@@ -16,23 +30,33 @@ export default function Navbar() {
         <h3 className='font-bold font-inter'>Weather</h3>
       </div>
       <div className='flex-center'>
-        {isSignedIn ? (
+        {session ? (
           <div className='flex-center gap-3'>
             <button className='btn'>
               Profile
             </button>
             <Image
-              src={logo}
+              src={session.user.image}
               alt='Website logotype'
-              width={40} 
-              height={40} 
+              width={40}
+              height={40}
+              className='rounded-full'
             />
           </div>
         ) : (
           <div className='flex-center'>
-            <button className='btn'>
-              Sign in
-            </button>
+            {providers && 
+              Object.values(providers).map((provider: Provider | any) => (
+                <button 
+                  className='btn' 
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                >
+                  Sign in
+                </button>
+              ))}
           </div>
         )}
       </div>
