c396d0f37edcba73ba82b1b58dbb3cc04e48cb92
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-06-04 23:33:40 +0600

    Added weather images contant

---
+++ src/constants/weatherImages.ts
@@ -0,0 +1,20 @@
+// images for day weather
+import day1 from '@/public/weather/day1.svg';
+import day2 from '@/public/weather/day2.svg';
+import day3 from '@/public/weather/day3.svg';
+import day4 from '@/public/weather/day4.svg';
+import day5 from '@/public/weather/day5.svg';
+import day6 from '@/public/weather/day6.svg';
+
+// images for night weather
+import night1 from '@public/weather/night1.svg';
+import night2 from '@public/weather/night2.svg';
+import night3 from '@public/weather/night3.svg';
+import night4 from '@public/weather/night4.svg';
+import night5 from '@public/weather/night5.svg';
+import night6 from '@public/weather/night6.svg';
+
+export const weatherImages = {
+  day: [day1, day2, day3, day4, day5, day6],
+  night: [night1, night2, night3, night4, night5, night6],
+}
