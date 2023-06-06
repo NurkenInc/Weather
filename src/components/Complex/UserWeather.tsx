4e6f7d438f85e5675615ec87b7df2e9b327ba106
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-06-06 23:31:27 +0600

    add user weather ui

---
+++ src/components/Complex/UserWeather.tsx
@@ -0,0 +1,9 @@
+import WeatherCard from '@components/Basic/WeatherCard';
+
+export default function UserWeather() {
+  return (
+    <div>
+      {/* <WeatherCard city='astana' temp='15C' /> */}
+    </div>
+  )
+}
